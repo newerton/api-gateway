ffb886ea3b555fc6da3793b296dab43dfe8fc097
Newerton Vargas de Araujo <newerton.araujo@gmail.com>
2022-02-14 20:57:46 -0300

    chore: update to env k8s

--- src/common/config/configuration.ts
+++ src/common/config/configuration.ts
@@ -1,12 +1,15 @@
 import { PolicyEnforcementMode, TokenValidation } from '../auth/keycloak';
-
 export default () => ({
+  kafka: {
+    brokers: `${process.env.KAFKA_BROKER_HOST}:${process.env.KAFKA_BROKER_PORT}`,
+  },
   keycloak: {
-    debug: process.env.DEBUG === '1',
-    baseUrl: `${process.env.KEYCLOAK_BASE_URL}/auth`,
+    debug: process.env.KEYCLOAK_DEBUG === '1',
+    baseInternalUrl: `${process.env.KEYCLOAK_BASE_INTERNAL_URL}/auth`,
+    baseExternalUrl: `${process.env.KEYCLOAK_BASE_EXTERNAL_URL}/auth`,
     realm: process.env.KEYCLOAK_REALM || '',
-    clientId: process.env.KEYCLOAK_CLIENT_ID || '',
-    secret: process.env.KEYCLOAK_SECRET || '',
+    clientId: process.env.KEYCLOAK_API_GATEWAY_CLIENT_ID || '',
+    secret: process.env.KEYCLOAK_API_GATEWAY_SECRET || '',
     publicKey: process.env.KEYCLOAK_PUBLIC_KEY || '',
     policyEnforcement: PolicyEnforcementMode.ENFORCING,
     tokenValidation: TokenValidation.OFFLINE,
